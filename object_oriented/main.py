2faee7991966707536546d6b97d8ed106748872a
Admin <borg@ubuntu.ubuntu-domain>
2013-04-30 16:30:17 +0200

    Bewegung Dinosaurier mit Class

--- object_oriented/main.py
+++ object_oriented/main.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.locals import *
 import cevent
-from spriteclasses import Dinosaur
+from spriteclasses import Dinosaur, Enemy
 
 class App(cevent.CEvent):
     def __init__(self):
@@ -12,16 +12,19 @@ class App(cevent.CEvent):
     
     def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode((350,350), pygame.HWSURFACE)
+        self._display_surf = pygame.display.set_mode((500,550), pygame.HWSURFACE)
         self._running = True
         self._image_surf = pygame.image.load("myimage.jpg").convert()
         self.dinosaur = Dinosaur()
+        self.enemy = Enemy()
     
     def on_loop(self):
         pass
+    
     def on_render(self):
         self._display_surf.blit(self._image_surf,(0,0))
         self._display_surf.blit(self.dinosaur.get_current_image(), self.dinosaur.rect)
+        self._display_surf.blit(self.enemy.picture, self.enemy.rect)
         pygame.display.flip()
     
     def on_exit(self):
@@ -29,7 +32,20 @@ class App(cevent.CEvent):
 
     def on_cleanup(self):
         pygame.quit()
-     
+    
+    def on_key_down(self, event):
+		if event.key == pygame.K_RIGHT:
+			print "key right"
+			self.dinosaur.moveright()
+		if event.key == pygame.K_LEFT:
+			print "key left"
+			self.dinosaur.moveleft()
+		if event.key == pygame.K_UP:
+			self.dinosaur.moveup()
+		if event.key == pygame.K_DOWN:
+			self.dinosaur.movedown()
+		#self.enemy.move()
+		
     def on_execute(self):
         if self.on_init() == False:
             self._running = False
--- object_oriented/spriteclasses.py
+++ object_oriented/spriteclasses.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 class Dinosaur(pygame.sprite.Sprite):
 	def __init__(self):
@@ -10,6 +11,7 @@ class Dinosaur(pygame.sprite.Sprite):
 		self.index = 0
 		self.rect = self.image.get_rect()
 		self.rect.center = [100, 100]
+		self.speed = random.randint(1, 10)
 	
 	def get_current_image(self):
 		return self.image
@@ -22,3 +24,24 @@ class Dinosaur(pygame.sprite.Sprite):
 		self.index += 1
 		self.index = self.index % len(self.images)
 		self.image = self.images[self.index]
+	def moveleft(self):
+		self.rect.center = (self.rect.center[0] - self.speed, self.rect.center[1])
+	def moveright(self):
+		self.rect.center = (self.rect.center[0] + self.speed, self.rect.center[1])
+	def moveup(self):
+		self.rect.center = (self.rect.center[0] , self.rect.center[1] - self.speed)
+	def movedown(self):
+		self.rect.center = (self.rect.center[0] , self.rect.center[1] + self.speed)	
+		
+class Enemy (pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.picture = pygame.image.load('artwork/Esteban3.png')
+		self.rect = self.picture.get_rect()
+		self.rect.center = [100, 500]
+		self.speed = random.randint(1, 10)
+		
+	def move(self):
+		self.rect.center = (self.rect.center[0] - self.speed, self.rect.center[1])
+	
+		
