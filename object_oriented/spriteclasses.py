49766997b3be5809a99cd5af4cbbe8c33746ff89
Dominik Bartenstein <db@wahuu.at>
2013-05-14 11:30:51 +0200

    work in progress

--- object_oriented/spriteclasses.py
+++ object_oriented/spriteclasses.py
@@ -2,37 +2,50 @@ import pygame
 import random
 
 class Dinosaur(pygame.sprite.Sprite):
-	def __init__(self):
-		pygame.sprite.Sprite.__init__(self)
-		image = pygame.image.load('artwork/dino1.png')
-		image2 = pygame.image.load('artwork/dino2.png')
-		self.images = [image, image2]
-		self.image = self.images[0]
-		self.index = 0
-		self.rect = self.image.get_rect()
-		self.rect.center = [100, 100]
-		self.speed = random.randint(1, 10)
-	
-	def get_current_image(self):
-		return self.image
-	
-	def process_event(self, event):
-		if event.type == pygame.USEREVENT:
-			self.next_image()
-			
-	def next_image(self):
-		self.index += 1
-		self.index = self.index % len(self.images)
-		self.image = self.images[self.index]
-	def moveleft(self):
-		self.rect.center = (self.rect.center[0] - self.speed, self.rect.center[1])
-	def moveright(self):
-		self.rect.center = (self.rect.center[0] + self.speed, self.rect.center[1])
-	def moveup(self):
-		self.rect.center = (self.rect.center[0] , self.rect.center[1] - self.speed)
-	def movedown(self):
-		self.rect.center = (self.rect.center[0] , self.rect.center[1] + self.speed)	
-		
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        image_1 = pygame.image.load('artwork/dino1.png').convert_alpha()
+        image_2 = pygame.image.load('artwork/dino2.png').convert_alpha()
+        self.images = [image_1, image_2]
+        self.image = self.images[0]
+        self.index = 0
+        self.rect = self.image.get_rect()
+        self.rect.center = [100, 100]
+        self.speed = random.randint(1, 10)
+
+    def get_current_image(self):
+        return self.image
+
+    def process_event(self, event):
+        if event.type == pygame.USEREVENT:
+            self.next_image()
+            
+    def next_image(self):
+        self.index += 1
+        self.index = self.index % len(self.images)
+        self.image = self.images[self.index]
+        
+    def moveleft(self):
+        self.rect.center = (self.rect.center[0] - self.speed, self.rect.center[1])
+
+    def moveright(self):
+        self.rect.center = (self.rect.center[0] + self.speed, self.rect.center[1])
+
+    def moveup(self):
+        self.rect.center = (self.rect.center[0] , self.rect.center[1] - self.speed)
+
+    def movedown(self):
+        self.rect.center = (self.rect.center[0] , self.rect.center[1] + self.speed)
+
+    def increase_size(self):
+        # TODO: increase size of dinosaur
+        pass
+
+    def decrease_size(self):
+        # TODO: decrease size of dinosaur
+        pass
+        
+        
 class Enemy(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
